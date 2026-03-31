6b34bfc27cb795d44dc8dace8d21fd59c56eb873
Brigitte Vinces <josselyn.gv27@gmail.com>
2026-03-30 22:00:25 -0500

    feat: implementar cambio dinámico de imagen y pie de foto del Poodle según la variedad seleccionada en el formulario

--- script.js
+++ script.js
@@ -1,6 +1,49 @@
 // 1. Seleccionamos el formulario del HTML
 const formulario = document.querySelector('form');
 const contenedorMensaje = document.getElementById('mensaje-confirmacion');
+const selectVariedad = document.getElementById('variedad');
+const imagenPoodle = document.getElementById('poodle-dinamico');
+const captionPoodle = document.getElementById('caption-dinamico');
+
+const infoPoodles = {
+    'gigante': {
+        src: 'img/Pgigante.jpeg',
+        text: 'Poodle Gigante: Elegancia en gran tamaño.'
+    },
+    'mediano': {
+        src: 'img/Poodle.jpeg',
+        text: 'Poodle Mediano: El equilibrio perfecto.'
+    },
+    'miniatura': {
+        src: 'img/Pminiatura.jpeg',
+        text: 'Poodle Miniatura: Pequeño pero activo.'
+    },
+    'toy': {
+        src: 'img/Ptoy.jpeg',
+        text: 'Poodle Toy: El consentido de la casa.'
+    },
+    '': { // Opción por defecto
+        src: 'img/principal.jpeg',
+        text: 'Poodle estándar mostrando su corte distintivo'
+    }
+};
+
+// 3. Escuchamos el evento 'change' (cuando cambian la selección)
+selectVariedad.addEventListener('change', function(event) {
+    // Obtenemos el valor seleccionado ('gigante', 'toy', etc.)
+    const variedadSeleccionada = event.target.value;
+    const info = infoPoodles[variedadSeleccionada];
+
+    imagenPoodle.style.opacity = 0;
+
+    setTimeout(() => {
+        imagenPoodle.src = info.src;
+        imagenPoodle.alt = `imagen de poodle ${variedadSeleccionada}`;
+        captionPoodle.textContent = `Figura 1: ${info.text}`;
+        imagenPoodle.style.opacity = 1;
+    }, 400);
+
+});
 
 // 2. Escuchamos cuando el usuario haga clic en "Reservar cita"
 formulario.addEventListener('submit', function(event) {
